63f5b412fff178460eb5e76d0b0878c88bd336f4
nyhunter77 <nyhunter77@yahoo.com>
2015-01-22 17:42:48 -0500

    angular Hello World

---
+++ app.js
@@ -0,0 +1,6 @@
+var homeworkModule = angular.module("homework", []);
+
+homeworkModule.controller("hwcontroller", function($scope) {
+  $scope.message = "Heeeeeeeelllllllooooooooo Wooooooooorrrrrrllllllldddddd!!!!!!";
+
+});
